f41cbdee3724206403cea0bcb4b816c523b71f03
yukogithub <netteoisii@gmail.com>
2023-03-03 22:46:21 +0900

    Lesson16 課題

--- src/main/java/com/techacademy/CountryController.java
+++ src/main/java/com/techacademy/CountryController.java
@@ -50,10 +50,13 @@ public class CountryController {
     }
 
     // ----- 削除画面 -----
-    @GetMapping("/delete")
-    public String deleteCountryForm(Model model) {
+    @GetMapping(value = {"/delete", "/delete/{code}/"})
+    public String deleteCountryForm(@PathVariable(name = "code", required = false) String code, Model model) {
+        // Modelに登録
+        model.addAttribute("code",code);
         // countrydelete.htmlに画面遷移
         return "country/delete";
+
     }
 
     // ----- 削除 -----
